c880cfacc0bc51362b4850b0e1b56b32aaa1e1af
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-02-13 08:33:57 +0000

    updated main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const myElement = <h1>I Love JSX 🫶!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
